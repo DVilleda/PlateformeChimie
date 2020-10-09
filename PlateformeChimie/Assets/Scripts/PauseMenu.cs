8aaa5c50ffb832a9f5b6d1e0fd642b5af5c5e3a2
Danny Villeda <1341188@crosemont.qc.ca>
2020-10-08 23:39:38 -0400

    Version Initiale de l

---
+++ PlateformeChimie/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject optionsScreen, pauseScreen;
+
+    public string mainMenuScene;
+
+    private bool isPaused;
+
+    public GameObject loadingScreen, loadingIcon;
+    public Text loadingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if(!isPaused)
+        {
+            pauseScreen.SetActive(true);
+            isPaused = true;
+
+            Time.timeScale = 0f;
+        } else
+        {
+            pauseScreen.SetActive(false);
+            isPaused = false;
+
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+    }
+
+    public void QuitToMain()
+    {
+        //SceneManager.LoadScene(mainMenuScene);
+
+        //Time.timeScale = 1f;
+
+        StartCoroutine(LoadMain());
+    }
+
+    public IEnumerator LoadMain()
+    {
+        loadingScreen.SetActive(true);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(mainMenuScene);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if(asyncLoad.progress >= .9f)
+            {
+                loadingText.text = "Press any key to continue";
+                loadingIcon.SetActive(false);
+
+                if(Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+
+                    Time.timeScale = 1f;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ PlateformeChimie/Assets/Scripts/SelectionNiveau.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectionNiveau : MonoBehaviour
+{
+    public string niveauChoisi;
+
+    public GameObject loadingScreen, loadingIcon;
+    public Text loadingText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){ }
+
+    public void commencerNiveau() {
+        StartCoroutine(LoadStart());
+    }
+
+    public IEnumerator LoadStart()
+    {
+        loadingScreen.SetActive(true);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(niveauChoisi);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress >= .9f)
+            {
+                loadingText.text = "Press any key to continue";
+                loadingIcon.SetActive(false);
+
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+
+                    Time.timeScale = 1f;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
