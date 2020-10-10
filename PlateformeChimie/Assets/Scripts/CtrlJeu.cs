3056d793609cf1116127532915b2dd4ad738694e
Danny Villeda <1341188@crosemont.qc.ca>
2020-10-09 20:37:19 -0400

    Ajout du personnage avec les animations de courir,sauter,idle et blesser. Ajout des components au joueur. Ajout des script de personnage et le script de jeu.

---
+++ PlateformeChimie/Assets/Scripts/CtrlJeu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlJeu : MonoBehaviour
+{
+    PersonnageCtrl personnageCtrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        personnageCtrl = GameObject.Find("Personnage").GetComponent<PersonnageCtrl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        personnageCtrl.deplacer(Input.GetAxis("Horizontal"));
+
+        if (Input.GetAxis("Vertical") > 0) {
+            personnageCtrl.sauter();
+        }
+    }
+}
