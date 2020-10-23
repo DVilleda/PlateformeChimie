2d71cc2fdbaf6e6844f5f11534cac934f6011184
Danny Villeda <1341188@crosemont.qc.ca>
2020-10-22 22:56:30 -0400

    Ajout de BGM dans les niveaux et changement au contenu des leçons de chimie. Création de dossier pour les scripts pour les regrouper mieux.

---
+++ PlateformeChimie/Assets/Scripts/Script Menus/MainMenu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public string SelectionNiveau;
+
+    public GameObject optionsScreen;
+
+    public GameObject loadingScreen, loadingIcon;
+    public Text loadingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadStart());
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public IEnumerator LoadStart()
+    {
+        loadingScreen.SetActive(true);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SelectionNiveau);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress >= .9f)
+            {
+                loadingText.text = "Appuire une touche pour continuer";
+                loadingIcon.SetActive(false);
+
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+
+                    Time.timeScale = 1f;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
